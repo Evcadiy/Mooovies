7ec6045cde6c0bad5ede2a595516b6601d5510b4
Evcadiy <95449216+Evcadiy@users.noreply.github.com>
2023-03-12 18:12:39 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,17 +22,12 @@
             }));
         }
     }), 0);
-    const API_KEY = "2a74575afamsh37cfe83865dbcacp108f04jsnf4261c2aedbf";
-    const API_HOST = "imdb-top-100-movies.p.rapidapi.com";
-    const API_URl = "https://imdb-top-100-movies.p.rapidapi.com/";
+    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+    const API_URl = "https://api.themoviedb.org/3/movie/popular?api_key=9bf74e379025d3a5253c5928f78a4a11&language=en-US&page=1";
+    const API_URL_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=9bf74e379025d3a5253c5928f78a4a11&language=en-US&page=1&include_adult=false&query=";
     getMovies(API_URl);
     async function getMovies(url) {
-        const resp = await fetch(url, {
-            headers: {
-                "X-RapidAPI-Key": API_KEY,
-                "X-RapidAPI-Host": API_HOST
-            }
-        });
+        const resp = await fetch(url);
         const respData = await resp.json();
         console.log(respData);
         showMovies(respData);
@@ -42,13 +37,25 @@
     }
     function showMovies(data) {
         const moviesEl = document.querySelector(".movies");
-        data.forEach((movie => {
+        document.querySelector(".movies").innerHTML = "";
+        data.results.forEach((movie => {
+            const {title, poster_path, vote_average, overview, genre_ids, release_date, id} = movie;
             const movieEl = document.createElement("div");
             movieEl.classList.add("movie");
-            movieEl.innerHTML = `\n    <div class="movie__cover-inner">\n      <img src="${movie.image}" alt="" class="movie_cover">\n      <div class="movie__cover--darkened"></div>\n    </div>\n    <div class="movie__info">\n      <div class="movie__title">${movie.title}</div>\n      <div class="movie__category">${movie.genre}</div>\n      <div class="movie__average movie__average--${getClassByVote(movie.rating)}">${movie.rating}</div>\n    </div>\n        `;
+            movieEl.innerHTML = `\n    <div class="movie__cover-inner">\n      <img src="${movie.poster_path ? IMG_PATH + poster_path : "https://i.pinimg.com/originals/bf/7a/3b/bf7a3b774d9dde18953fbea669a2a23a.jpg"}" alt="" class="movie_cover">\n      <div class="movie__cover--darkened"></div>\n    </div>\n    <div class="movie__info">\n      <div class="movie__title">${movie.title}</div>\n      <div class="movie__category"></div>\n      <div class="movie__average movie__average--${getClassByVote(movie.vote_average)}">${movie.vote_average}</div>\n    </div>\n        `;
             moviesEl.appendChild(movieEl);
         }));
     }
+    const script_form = document.querySelector("form");
+    const search = document.querySelector(".header__search");
+    script_form.addEventListener("submit", (e => {
+        e.preventDefault();
+        const apiSearch = `${API_URL_SEARCH}${search.value}`;
+        if (search.value) {
+            getMovies(apiSearch);
+            search.value = "";
+        }
+    }));
     window["FLS"] = true;
     isWebp();
 })();
